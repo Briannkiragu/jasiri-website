0a8a5a44e731cfa971d1ac05fa6fb3bfd44e23d9
Brian Kiragu <brianngaruiya03@gmail.com>
2026-04-27 18:11:03 +0300

    Add files via upload

---
+++ bravej.js
@@ -0,0 +1,15 @@
+const bar = document.getElementById('bar');
+const nav = document.getElementById('navbar');
+
+if(bar){
+bar.addEventListener('click', () =>{
+    nav.classList.add('active')
+
+
+
+
+})
+
+
+
+}
